4f737f82705414c52828a3ee013a81b56037d99b
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-07-07 12:00:00 -0300

    Fixed template generation

--- src/main/g8/modules/$if(include_data_l1.truthy)$shared$endif$/src/main/scala/$package$/shared/Main.scala
+++ src/main/g8/modules/$if(include_data_l1.truthy)$shared$endif$/src/main/scala/$package$/shared/Main.scala
@@ -1,10 +1,5 @@
-package $package$.l1
+package $package$.shared
 
-import java.util.UUID
-import org.tessellation.BuildInfo
-import org.tessellation.currency.l1.CurrencyL1App
-import org.tessellation.schema.cluster.ClusterId
-
-object Main {
+object Data {
 
 }
