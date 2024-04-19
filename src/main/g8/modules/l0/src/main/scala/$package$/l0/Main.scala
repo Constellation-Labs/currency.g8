cb0219a5e5e89a18b818a2bbd44505454c42735c
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-04-19 11:16:46 -0300

    Updating signatures

--- src/main/g8/modules/l0/src/main/scala/$package$/l0/Main.scala
+++ src/main/g8/modules/l0/src/main/scala/$package$/l0/Main.scala
@@ -6,6 +6,7 @@ import org.tessellation.currency.dataApplication.{BaseDataApplicationL0Service}
 import org.tessellation.currency.l0.CurrencyL0App
 import org.tessellation.schema.cluster.ClusterId
 import org.tessellation.security.SecurityProvider
+import org.tessellation.schema.semver.{MetagraphVersion, TessellationVersion}
 
 import java.util.UUID
 
@@ -14,6 +15,7 @@ import java.util.UUID
       "$name;format="lower,hyphen"$-l0",
       "$name$ L0 node",
       ClusterId(UUID.fromString("517c3a05-9219-471b-a54c-21b7d72f4ae5")),
-      version = BuildInfo.version
+      tessellationVersion = TessellationVersion.unsafeFrom(BuildInfo.version),
+      metagraphVersion = MetagraphVersion.unsafeFrom(BuildInfo.version)
     ) {
   }
